312a34777c29eaf1d7b61320a376014ee00167d1
shichiku_yuki <shichiku_yuki@cyberagent.co.jp>
2015-06-16 19:47:24 +0900

    Fixed cache

--- httpRequest/routes/users.js
+++ httpRequest/routes/users.js
@@ -2,6 +2,20 @@ var express = require('express');
 // express のルーティングを決めるときに使う。(この辺りが参考になります http://expressjs.com/guide/routing.html )
 var router = express.Router();
 
+// post送信するページを用意
+router.get('/form', function(req, res) {
+  res.render('userForm');
+});
+
+// 最後にPOSTされたcacheを確認する
+router.get('/cache', function(req, res){
+  if (cache === undefined) {
+    return res.send('キャッシュがありません');
+  }
+  return res.send('最後にPOSTされたデータのcache : ' + cache);
+});
+
+
 // /users にただのGETリクエストが来た時は文字を返す
 router.get('/', function(req, res) {
   res.send('/users にGETリクエストが来たよ！');
@@ -22,11 +36,6 @@ router.get('/:id/:name', function(req, res) {
 
 
 
-// post送信するページを用意
-router.get('/form', function(req, res) {
-  res.render('userForm');
-});
-
 /**
  * /users へのリクエストがPOSTでidと一緒に来たら
  * 最後にアクセスした人のキャッシュを書き換える
@@ -37,11 +46,12 @@ router.post('/', function(req, res) {
   // サーバーで持っているcacheという変数にPOSTされたbodyに入っているIDを入れる
   var id = req.body.id;
   if (id === '') {
-    res.send('入力が不正です');
+    return res.send('入力が不正です');
   }
   // セキュリティ対策 ※サニタイズ
   cache = validator.escape(id);
-  res.send('最後にPOSTしたIDは ' + cache + 'です！');
+  //cache = id; // これだとどうなるか試してみましょう。 <script>aleat('XSS!')</script>とかを入力してPOSTしてみてください
+  res.send('POSTされたIDは ' + cache + 'です！');
 });
 
 module.exports = router;
